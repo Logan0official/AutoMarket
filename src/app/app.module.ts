d71612489eddc47e9628f60a6f40418c36b7ea25
Logan0official <63509013+Logan0official@users.noreply.github.com>
2021-02-20 21:33:09 +0200

    home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { SliderComponent } from './pages/home/slider/slider.component';
+import { SliderItemDirective } from './pages/home/slider/slider-item.directive';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    SliderComponent,
+    SliderItemDirective
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
